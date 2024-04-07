b1f2e4b75d8442fd5b98694f6fb749c2cc685225
Archer <shanebigornia123@gmail.com>
2024-04-07 09:54:05 +0800

    added timer

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,22 +5,49 @@ var login = document.querySelector("#submit");
 var readme = document.querySelector(".inactivelink");
 let a = 0;
 
-function checkInput(){
+var countDownTime = 90;
 
-    
+function checkInput(){
+  
     if((text.value=="admin")&&pass.value=="admin123"){
         document.querySelector(".signin").style.display = "none";
-        // readme.style.display = "none";
-        
+        document.querySelector("#readmore").classList.remove('disabled');
+
         alert("Login successful!");
-        window.open("/public/assets/pup.html");
+        window.open("/public/assets/pup.html");      
         a++;
     }else{
         a++;
         if(a == 3){
             //set timer when number of attempts reached
-            //setTimeout
-            alert("maximum atttempt reached, try again later!");
+  
+            alert("Maximum atttempt reached, try again later!");
+            document.querySelector(".signin").style.display = "none";
+            //button change
+            document.querySelector(".w_auth").style.cursor = 'none';
+            document.querySelector(".w_auth").classList.add('disabled');
+
+            var timer = setInterval(function(){
+                
+            var currentTime = countDownTime --;
+            
+            if(currentTime == 1){
+                document.querySelector(".w_auth").innerHTML = currentTime + " second left.";
+            }
+
+            document.querySelector(".w_auth").innerHTML = currentTime + " seconds left.";
+                
+                if(currentTime < 0){
+                    clearInterval(timer);
+                    document.querySelector(".w_auth").classList.remove('disabled');
+                    document.querySelector(".w_auth").style.cursor = 'pointer';
+                    a = 0;
+                    document.querySelector(".w_auth").innerHTML = "Click me!&#128519;";
+                }
+                
+            }, 1000)
+
+
         }else{
             alert("Invalid credentials! Try again!, You still have " + (3 - a) + " attempts.");
         }
